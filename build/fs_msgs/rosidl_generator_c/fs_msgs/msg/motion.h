53e2720bcd0ceb242e1d2ee223806a57cee161e4
yasmin-nunes <yasmin.nunes@amperaracing.com>
2025-02-26 15:08:00 -0300

    Motion msg

---
+++ build/fs_msgs/rosidl_generator_c/fs_msgs/msg/motion.h
@@ -0,0 +1,12 @@
+// generated from rosidl_generator_c/resource/idl.h.em
+// with input from fs_msgs:msg/Motion.idl
+// generated code does not contain a copyright notice
+
+#ifndef FS_MSGS__MSG__MOTION_H_
+#define FS_MSGS__MSG__MOTION_H_
+
+#include "fs_msgs/msg/detail/motion__struct.h"
+#include "fs_msgs/msg/detail/motion__functions.h"
+#include "fs_msgs/msg/detail/motion__type_support.h"
+
+#endif  // FS_MSGS__MSG__MOTION_H_
